9db2775052af60d011f665a76ad70ea2a41e5ca3
tsu-nera <fox10225fox@gmail.com>
2022-02-19 17:05:31 +0900

    :hammer:replace assemble to tools.build

--- build.clj
+++ build.clj
@@ -6,26 +6,52 @@
 
 (def target-dir "target")
 (def class-dir (str target-dir "/" "classes"))
-(def jar-file (format "%s/%s-%s.jar" target-dir (name lib) version))
+(def artifacts-dir (str target-dir "/" "artifacts"))
+(def jar-file (format "%s/%s-%s-application.jar" artifacts-dir (name lib) version))
+(def uber-file (format "%s/%s-%s-standalone.jar" artifacts-dir (name lib) version))
 
-(def src ["src/main/clojure"])
+(def src-clj ["src/main/clojure"])
+(def src-java ["src/main/java"])
+(def src [src-clj src-java])
 (def basis (b/create-basis {:project "deps.edn"}))
 
 ;; clj -T:build clean
 (defn clean [_]
   "Delete the build target directory"
-  (println (str "Cleaning " target-dir))
+  (println (str "Cleaning... " target-dir))
   (b/delete {:path "target"}))
+
+(defn compile-java [_]
+  (println (str "Compiling... " src-java))
+  (b/javac {:src-dirs   src-java
+            :class-dir  class-dir
+            :basis      basis
+            :javac-opts ["-source" "11" "-target" "11" "-Xlint:all"]
+            }))
 
 ;; clj -T:build jar
 (defn jar [_]
-  "Create the jar from a source pom and source files"
   (b/write-pom {:class-dir class-dir
                 :lib       lib
                 :version   version
                 :basis     basis
-                :src-dirs  src})
-  (b/copy-dir {:src-dirs   src
+                :src-dirs  src-clj})
+  (b/copy-dir {:src-dirs   src-clj
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
           :jar-file  jar-file}))
+
+;; clj -T:build uber
+(defn uber [_]
+  (clean nil)
+  (compile-java nil)
+  ;; (b/copy-dir {:src-dirs   src-clj
+  ;;              :target-dir class-dir})
+  ;; (b/compile-clj {:basis     basis
+  ;;                 :src-dirs  src-clj
+  ;;                 :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis     basis
+           ;; :main      'functions.helloworld
+           }))
